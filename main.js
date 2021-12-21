2edbd140018ca137e73dc7eb1684903f063830fa
EvandersonXavier <evanderson10017@gmail.com>
2021-12-21 02:25:48 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,102 @@
+const $pedra = document.getElementById('pedra');
+const $pedraNpc = document.getElementById('pedraNpc');
+
+const $papel = document.getElementById('papel');
+const $papelNpc = document.getElementById('papelNpc');
+
+const $tesoura = document.getElementById('tesoura');
+const $tesouraNpc = document.getElementById('tesouraNpc');
+
+const $reset = document.getElementById('reset');
+
+/*const luck = Math.floor(Math.random() * 3);*/
+
+function pedra() {
+   const luck = Math.floor(Math.random() * 3);
+   switch(luck){
+       case 0:
+            $papel.style.display= "none";
+            $tesoura.style.display= "none";
+            $papelNpc.style.display= "flex";
+           break;
+       case 1:
+            $papel.style.display= "none";
+            $tesoura.style.display= "none";
+            $tesouraNpc.style.display= "flex";
+           break;
+       case 2:
+            $papel.style.display= "none";
+            $tesoura.style.display= "none";
+            $pedraNpc.style.display= "flex";
+           break;
+   }
+}
+
+function papel() {
+    const luck = Math.floor(Math.random() * 3);
+    switch(luck){
+        case 0:
+             $pedra.style.display= "none";
+             $tesoura.style.display= "none";
+             $papelNpc.style.display= "flex";
+            break;
+        case 1:
+             $pedra.style.display= "none";
+             $tesoura.style.display= "none";
+             $tesouraNpc.style.display= "flex";
+            break;
+        case 2:
+             $pedra.style.display= "none";
+             $tesoura.style.display= "none";
+             $pedraNpc.style.display= "flex";
+            break;
+    }
+ }
+
+ function tesoura() {
+    const luck = Math.floor(Math.random() * 3);
+    switch(luck){
+        case 0:
+             $papel.style.display= "none";
+             $pedra.style.display= "none";
+             $papelNpc.style.display= "flex";
+            break;
+        case 1:
+             $papel.style.display= "none";
+             $pedra.style.display= "none";
+             $tesouraNpc.style.display= "flex";
+            break;
+        case 2:
+             $papel.style.display= "none";
+             $pedra.style.display= "none";
+             $pedraNpc.style.display= "flex";
+            break;
+    }
+ }
+
+ function reset(){
+    $pedra.style.display= "flex";
+    $papel.style.display= "flex";
+    $tesoura.style.display= "flex";
+    $pedraNpc.style.display= "none";
+    $papelNpc.style.display= "none";
+    $tesouraNpc.style.display= "none";
+ }
+
+/*function pedra() {
+    $pedraNpc.style.display= "none";
+    $papelNpc.style.display= "flex";
+    $tesouraNpc.style.display= "none";
+}
+
+function papel(){
+    $pedraNpc.style.display= "none";
+    $papelNpc.style.display= "none";
+    $tesouraNpc.style.display= "flex";
+}
+
+function tesoura() {
+    $pedraNpc.style.display= "flex";
+    $papelNpc.style.display= "none";
+    $tesouraNpc.style.display= "none";
+}*/
